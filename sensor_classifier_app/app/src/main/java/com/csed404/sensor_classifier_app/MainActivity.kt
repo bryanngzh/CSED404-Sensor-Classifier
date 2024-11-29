6fe460a91c1a29e550d214279d173ba56cdf5878
bryanngzh <bryanngzihao@gmail.com>
2024-11-29 22:37:00 +0900

    fix: start, stop, start bug

--- sensor_classifier_app/app/src/main/java/com/csed404/sensor_classifier_app/MainActivity.kt
+++ sensor_classifier_app/app/src/main/java/com/csed404/sensor_classifier_app/MainActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -31,7 +32,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var classifiedActivityTextView: TextView
     private lateinit var startStopButton: Button
 
-    private val coroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    private var coroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,6 +56,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             if (isClassifying) {
                 stopClassification()
             } else {
+                if (coroutineScope.isActive.not()) {
+                    coroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+                }
                 startClassification()
             }
         }
